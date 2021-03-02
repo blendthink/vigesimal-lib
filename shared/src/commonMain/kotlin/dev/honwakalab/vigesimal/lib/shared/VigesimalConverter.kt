d529d06e7a867854a66cdfb2e057f84ed42c40dc
blendthink <info@blendthink.dev>
2021-03-03 08:06:24 +0900

    change mapping type to BigDecimal

--- shared/src/commonMain/kotlin/dev/honwakalab/vigesimal/lib/shared/VigesimalConverter.kt
+++ shared/src/commonMain/kotlin/dev/honwakalab/vigesimal/lib/shared/VigesimalConverter.kt
@@ -8,35 +8,34 @@ import com.ionspin.kotlin.bignum.decimal.toBigDecimal
 /**
  * 20進法のマッピング
  */
-private val MAPPING_VIGESIMAL: Map<String, Int> = mapOf(
-    "0" to 0,
-    "1" to 1,
-    "2" to 2,
-    "3" to 3,
-    "4" to 4,
-    "5" to 5,
-    "6" to 6,
-    "7" to 7,
-    "8" to 8,
-    "9" to 9,
-    "a" to 10,
-    "b" to 11,
-    "c" to 12,
-    "d" to 13,
-    "e" to 14,
-    "f" to 15,
-    "g" to 16,
-    "h" to 17,
-    "i" to 18,
-    "j" to 19,
+private val MAPPING_VIGESIMAL: Map<String, BigDecimal> = mapOf(
+    "0" to BigDecimal.fromInt(0),
+    "1" to BigDecimal.fromInt(1),
+    "2" to BigDecimal.fromInt(2),
+    "3" to BigDecimal.fromInt(3),
+    "4" to BigDecimal.fromInt(4),
+    "5" to BigDecimal.fromInt(5),
+    "6" to BigDecimal.fromInt(6),
+    "7" to BigDecimal.fromInt(7),
+    "8" to BigDecimal.fromInt(8),
+    "9" to BigDecimal.fromInt(9),
+    "a" to BigDecimal.fromInt(10),
+    "b" to BigDecimal.fromInt(11),
+    "c" to BigDecimal.fromInt(12),
+    "d" to BigDecimal.fromInt(13),
+    "e" to BigDecimal.fromInt(14),
+    "f" to BigDecimal.fromInt(15),
+    "g" to BigDecimal.fromInt(16),
+    "h" to BigDecimal.fromInt(17),
+    "i" to BigDecimal.fromInt(18),
+    "j" to BigDecimal.fromInt(19),
 )
 
 object VigesimalConverter {
     fun toDecimal(vigesimal: String): BigDecimal {
         val length = vigesimal.length - 1
         return vigesimal.mapIndexed { index, c ->
-            val decimal = MAPPING_VIGESIMAL[c.toString()] ?: throw IllegalArgumentException("$c")
-            val bigDecimal = decimal.toBigDecimal()
+            val bigDecimal = MAPPING_VIGESIMAL[c.toString()] ?: throw IllegalArgumentException("$c")
             val powOf86 = 20.toBigDecimal().pow(length - index)
             return@mapIndexed (bigDecimal * powOf86)
         }.sumOf { it }
